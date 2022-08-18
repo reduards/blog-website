eb7219a27440f01c336d7662a585cb8b092fcfb7
reduards <43570068+reduards@users.noreply.github.com>
2022-08-18 08:06:03 +0000

    removed comment

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -32,7 +32,6 @@ const ContactPage = ({
             Your message:
             <textarea name="message"></textarea>
           </label>
-          <!-- your other form fields go here -->
           <button type="submit">Send</button>
         </form>
         </div>
